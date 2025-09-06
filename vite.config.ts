abb8177efd2405b55aecb105c5c760938238d68e
1571685211@qq.com <1571685211@qq.com>
2025-09-06 19:50:49 +0800

    config: 代码提交规范添加

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import legacy from '@vitejs/plugin-legacy' // 导入插件
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { createHtmlPlugin } from 'vite-plugin-html'
+// import { createHtmlPlugin } from 'vite-plugin-html'
 import svgr from 'vite-plugin-svgr'
 
 // // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     plugins: [
       tanstackRouter({ target: 'react', autoCodeSplitting: true }),
@@ -27,30 +27,16 @@ export default defineConfig(({ mode }) => {
       legacy({
         targets: ['defaults', 'not IE 11'],
       }),
-      createHtmlPlugin({
-        template: './index.html', // 关联 HTML 模板
-        inject: {
-          data: {
-            // 定义需要注入的 JS 资源（支持数组形式，方便循环）
-            injectScripts: [
-              mode === 'development' ? 'https://cdn.example.com/prod-script.js' : '',
-            ].filter(Boolean), // 过滤空值（避免生成空的 script 标签）
-          },
-          // 可选：指定注入位置（'head' | 'body-prepend' | 'body-append'）
-          // injectTo: 'head',
-        },
-      }),
     ],
-    resolve: {
-
-    },
+    resolve: {},
     server: {
       open: true,
     },
     build: {
       sourcemap: false,
       rollupOptions: {
-        output: { // 代码分割策略
+        output: {
+          // 代码分割策略
           manualChunks: {
             react: ['react', 'react-dom'], // 单独打包 React 相关库
             utils: ['lodash', 'ky'], // 工具库单独打包
